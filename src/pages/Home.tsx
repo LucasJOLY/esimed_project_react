6138f038b28123b3f64251306189d221df285f9b
Lucas JOLY <lucas.joly.0903@gmail.com>
2025-02-27 11:07:40 +0100

    install prestart of the project

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <div>
+      <h1>Page d'accueil</h1>
+    </div>
+  );
+}; 
---
+++ src/theme/MuiTheme.tsx
@@ -0,0 +1,34 @@
+import { createTheme } from "@mui/material/styles";
+import React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+const themeOptions = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#fff",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#3880ff",
+          borderRadius: 10,
+          color: "#fff",
+          "&:hover": {
+            backgroundColor: "#2d67d4",
+          },
+        },
+      },
+    },
+  },
+});
+
+const MuiThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  return <ThemeProvider theme={themeOptions}>{children}</ThemeProvider>;
+};
+
+export default MuiThemeProvider;
